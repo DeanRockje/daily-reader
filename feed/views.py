77a43d0b802133797483c1edb5a0d7a0d682143f
Dean Rockje <dice17nice@gmail.com>
2016-08-11 12:22:45 +0300

    Added form filtering

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-from .models import Feed,Entry
+from .models import Feed,Entry,Category
 from .forms import AddFeed, CreateCategory
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ValidationError
+from .tasks import update_feed
 
 
 # Create your views here.
@@ -16,12 +16,12 @@ def category_index(request):
 
 @login_required
 def feed_list(request,pk):
-    feeds = Feed.objects.filter(category__id = pk)
+    feeds = Feed.objects.filter(category__id = pk).order_by('-adding_date')
     return render(request,'feed/feed_list.html', {'feed':feeds})
 
 @login_required
 def entry_list(request,pk):
-    entries = Entry.objects.filter(feed__id=pk)
+    entries = Entry.objects.filter(feed__id=pk).order_by('-publication_date')
     return render(request,'feed/feed_items.html',{'entry':entries})
 
 @login_required
@@ -31,9 +31,10 @@ def add_feed(request):
         if form.is_valid():
             feed = form.save(commit=False)
             feed.save()
-            return HttpResponseRedirect('/category/')
+            return HttpResponseRedirect('/digest/')
     else:
         form=AddFeed()
+        form.fields['category'].queryset = Category.objects.filter(user=request.user)
     return render(request, 'feed/add_feed.html', {'form':form})
 
 @login_required
@@ -41,11 +42,18 @@ def create_category(request):
     if request.method == 'POST':
         form = CreateCategory(request.POST)
 
-        if form.is_valid:
+        if form.is_valid():
             category = form.save(commit=False)
             category.save()
-            return HttpResponseRedirect('/new/')
+            return HttpResponseRedirect('/digest/')
     else:
         form = CreateCategory()
     return render(request, 'feed/add_category.html', {'form':form})
 
+
+@login_required
+def manual_refresh(request, pk):
+    feed = Feed.objects.get(pk=pk)
+    update_feed(feed).delay()
+    entries = Entry.objects.filter(feed__id = pk)
+    return render(request,'feed/entry_list.html',{'entries':entries})
