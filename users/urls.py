ff19743966a07aeaf27046dcc64e898d42eedf5a
Dean Rockje <dice17nice@gmail.com>
2016-08-21 23:31:16 +0300

    Urls changed

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$',views.sign_in, name='login'),
+    url(r'^$',RedirectView.as_view(url='login'), name = 'redirect'),
+    url(r'^login/$',views.sign_in, name='login'),
     url(r'^logout/$',views.log_out, name='logout'),
     url(r'^create_user/$', views.RegisterNewUser, name='registration'),
 ]
